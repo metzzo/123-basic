c99af04f5ae4634e2198614f9bd67a8cf3d513e1
Robert Fischer <robert@programming-with-design.at>
2015-02-07 16:47:07 +0100

    resize multi dimensional array saves data correctly

--- 123basic.app/Libs/js/array.js
+++ 123basic.app/Libs/js/array.js
@@ -98,7 +98,7 @@ OTTArray.prototype.arrAccess = function() {
 function realArrSize(dims) {
 	var realSize = 1;
 	for(d in dims) {
-		dims[d] = CAST2INT(dims[d]);
+		dims[d] = ~~dims[d];
 		realSize *= dims[d];
 	}
 	return realSize;
@@ -122,7 +122,12 @@ function DIM(vari, dims, value) {
 function REDIM(vari, dims, value) {
 	var oldLength = vari.values.length;
 	var doDim = false, action;
-	vari.dimensions = dims;
+	var oldDim = vari.dimensions;
+	
+	if (!oldDim || oldDim.length !== vari.dimensions.length) {
+		doDim = true;
+	}
+	
 	if (vari.defval !== value) {
 		doDim = true;
 		if (value instanceof Array && !(vari.defval instanceof Array)) {
@@ -137,25 +142,66 @@ function REDIM(vari, dims, value) {
 	}
 	vari.defval = value;
 	
-	
-	//OBACHT könnte bei mehrdimensionalen arrys unerwartete ergebnisse liefern, wenn man elemente rauslöscht
-	vari.values.length = realArrSize(dims);
-	var start = oldLength;
-	if (doDim) start = 0;
-	for(i = start; i < vari.values.length; i++) {
-		if (vari.values[i]) {
-			if (action == 1) {
-				//Es muss ein Array sein
-				vari.values[i] = [vari.values[i]];
-			} else if (action == 2) {
-				//Es darf kein Array sein
-				vari.values[i] = vari.values[i][0];
+	if (doDim || vari.dimensions.length === 1) {
+		vari.dimensions = dims;
+		vari.values.length = realArrSize(dims);
+		
+		for(var i = doDim ? 0 : oldLength; i < vari.values.length; i++) {
+			if (vari.values[i]) {
+				if (action === 1) {
+					//Es muss ein Array sein
+					vari.values[i] = [vari.values[i]];
+				} else if (action === 2) {
+					//Es darf kein Array sein
+					vari.values[i] = vari.values[i][0];
+				}
+			} else {
+				//default wert geben
+				vari.values[i] = tryClone(vari.defval);
 			}
-		} else {
-			//default wert geben
-			vari.values[i] = tryClone(vari.defval);
 		}
+	} else {
+		// reposition stuff
+		var newOTTArray = new OTTArray(vari.defval);
+		DIM(newOTTArray, dims, vari.defval);
+		
+		var positionArray = new Array(oldDim.length);
+		for (var i = 0; i < positionArray.length; i++) { positionArray[i] = 0; }
+		
+		for (var i = 0; i < oldLength; i++) {
+			// copy value
+			var value = vari.arrAccess.apply(vari, positionArray).values[tmpPositionCache];
+			
+			if (action === 1) {
+				//Es muss ein Array sein
+				value = [value];
+			} else if (action === 2) {
+				//Es darf kein Array sein
+				value = value[0];
+			}
+			
+			// set value
+			try { // this should NOT be done with exception handling... VERY slow
+				newOTTArray.arrAccess.apply(newOTTArray, positionArray).values[tmpPositionCache] = value;
+			} catch(e) { }
+			
+			positionArray[0]++;
+			var overflow, j = 0;
+			do {
+				overflow = false;
+				if (positionArray[j] >= oldDim[j]) {
+					positionArray[j] = 0;
+					positionArray[j + 1]++;
+					overflow = true;
+					j++;
+				}
+			} while(overflow);
+		}
+		
+		vari.values = newOTTArray.values;
+		vari.dimensions = dims;
 	}
+	
 	
 	return vari;
 }
